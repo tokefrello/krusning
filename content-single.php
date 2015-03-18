202600b337f3bd5def3367e40c551c1fe0d917a1
tokefrello <tokefrello@gmail.com>
2015-03-18 17:46:07 +0100

    initialize with bare theme

---
+++ content-single.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package krusning
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+		<div class="entry-meta">
+			<?php krusning_posted_on(); ?>
+		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'krusning' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php krusning_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
